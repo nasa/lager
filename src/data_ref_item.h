16938d1a756cdcc7c45eb6311b36beaccfa32cde
Chuck Claunch <charles.a.claunch@nasa.gov>
2018-01-31 14:07:57 -0800

    intermediate branch to try and figure out templates issue

--- src/data_ref_item.h
+++ src/data_ref_item.h
@@ -1,11 +1,11 @@
 #ifndef DATA_REF_ITEM
 #define DATA_REF_ITEM
 
-#include <string>
-#include <stdint.h>
-#include <typeinfo>
-
-#include "lager_utils.h"
+// #include <string>
+// #include <stdint.h>
+// #include <typeinfo>
+// #include <stdexcept>
+#include <arpa/inet.h>
 
 struct AbstractDataRefItem
 {
@@ -32,7 +32,7 @@ class DataRefItem : public AbstractDataRefItem
 {
 public:
     DataRefItem(const std::string& name_in, T* dataRef_in):
-        AbstractDataRefItem(name_in, sizeof(T)),
+        AbstractDataRefItem(name_in, "unsupported", sizeof(T)),
         dataRef(dataRef_in)
     {
         throw std::runtime_error("unsupported data type");
@@ -51,174 +51,103 @@ private:
 };
 
 template<>
-class DataRefItem<uint8_t> : public AbstractDataRefItem
-{
-public:
-    DataRefItem(const std::string& name_in, uint8_t* dataRef_in):
-        AbstractDataRefItem(name_in, "uint8_t", sizeof(uint8_t)),
-        dataRef(dataRef_in) {}
-
-    ~DataRefItem() {}
-
-    void getNetworkDataRef(void* data)
-    {
-        *(uint8_t*)data = *reinterpret_cast<uint8_t*>(dataRef);
-    }
-
-private:
-    uint8_t* dataRef;
-};
+DataRefItem<uint8_t>::DataRefItem(const std::string& name_in, uint8_t* dataRef_in):
+    AbstractDataRefItem(name_in, "uint8_t", sizeof(uint8_t)),
+    dataRef(dataRef_in) {}
 
 template<>
-class DataRefItem<int8_t> : public AbstractDataRefItem
+void DataRefItem<uint8_t>::getNetworkDataRef(void* data)
 {
-public:
-    DataRefItem(const std::string& name_in, int8_t* dataRef_in):
-        AbstractDataRefItem(name_in, "int8_t", sizeof(int8_t)),
-        dataRef(dataRef_in) {}
-
-    ~DataRefItem() {}
-
-    void getNetworkDataRef(void* data)
-    {
-        *(int8_t*)data = *reinterpret_cast<int8_t*>(dataRef);
-    }
-
-private:
-    int8_t* dataRef;
-};
+    *(uint8_t*)data = *reinterpret_cast<uint8_t*>(dataRef);
+}
 
 template<>
-class DataRefItem<uint16_t> : public AbstractDataRefItem
-{
-public:
-    DataRefItem(const std::string& name_in, uint16_t* dataRef_in):
-        AbstractDataRefItem(name_in, "uint16_t", sizeof(uint16_t)),
-        dataRef(dataRef_in) {}
-
-    ~DataRefItem() {}
-
-    void getNetworkDataRef(void* data)
-    {
-        *(uint16_t*)data = htons(*reinterpret_cast<uint16_t*>(dataRef));
-    }
-
-private:
-    uint16_t* dataRef;
-};
+DataRefItem<int8_t>::DataRefItem(const std::string& name_in, int8_t* dataRef_in):
+    AbstractDataRefItem(name_in, "int8_t", sizeof(int8_t)),
+    dataRef(dataRef_in) {}
 
 template<>
-class DataRefItem<int16_t> : public AbstractDataRefItem
+void DataRefItem<int8_t>::getNetworkDataRef(void* data)
 {
-public:
-    DataRefItem(const std::string& name_in, int16_t* dataRef_in):
-        AbstractDataRefItem(name_in, "int16_t", sizeof(int16_t)),
-        dataRef(dataRef_in) {}
-
-    ~DataRefItem() {}
-
-    void getNetworkDataRef(void* data)
-    {
-        *(int16_t*)data = htons(*reinterpret_cast<int16_t*>(dataRef));
-    }
-
-private:
-    int16_t* dataRef;
-};
+    *(int8_t*)data = htons(*reinterpret_cast<int8_t*>(dataRef));
+}
 
 template<>
-class DataRefItem<uint32_t> : public AbstractDataRefItem
-{
-public:
-    DataRefItem(const std::string& name_in, uint32_t* dataRef_in):
-        AbstractDataRefItem(name_in, "uint32_t", sizeof(uint32_t)),
-        dataRef(dataRef_in) {}
-
-    ~DataRefItem() {}
-
-    void getNetworkDataRef(void* data)
-    {
-        *(uint32_t*)data = htonl(*reinterpret_cast<uint32_t*>(dataRef));
-    }
-
-private:
-    uint32_t* dataRef;
-};
+DataRefItem<uint16_t>::DataRefItem(const std::string& name_in, uint16_t* dataRef_in):
+    AbstractDataRefItem(name_in, "uint16_t", sizeof(uint16_t)),
+    dataRef(dataRef_in) {}
 
 template<>
-class DataRefItem<int32_t> : public AbstractDataRefItem
+void DataRefItem<uint16_t>::getNetworkDataRef(void* data)
 {
-public:
-    DataRefItem(const std::string& name_in, int32_t* dataRef_in):
-        AbstractDataRefItem(name_in, "int32_t", sizeof(int32_t)),
-        dataRef(dataRef_in) {}
-
-    ~DataRefItem() {}
-
-    void getNetworkDataRef(void* data)
-    {
-        *(int32_t*)data = htonl(*reinterpret_cast<int32_t*>(dataRef));
-    }
-
-private:
-    int32_t* dataRef;
-};
+    *(uint16_t*)data = htons(*reinterpret_cast<uint16_t*>(dataRef));
+}
 
 template<>
-class DataRefItem<uint64_t> : public AbstractDataRefItem
-{
-public:
-    DataRefItem(const std::string& name_in, uint64_t* dataRef_in):
-        AbstractDataRefItem(name_in, "uint64_t", sizeof(uint64_t)),
-        dataRef(dataRef_in) {}
-
-    ~DataRefItem() {}
-
-    void getNetworkDataRef(void* data)
-    {
-        *(uint64_t*)data = lager_utils::htonll(*reinterpret_cast<uint64_t*>(dataRef));
-    }
-
-private:
-    uint64_t* dataRef;
-};
+DataRefItem<int16_t>::DataRefItem(const std::string& name_in, int16_t* dataRef_in):
+    AbstractDataRefItem(name_in, "int16_t", sizeof(int16_t)),
+    dataRef(dataRef_in) {}
 
 template<>
-class DataRefItem<int64_t> : public AbstractDataRefItem
+void DataRefItem<int16_t>::getNetworkDataRef(void* data)
 {
-public:
-    DataRefItem(const std::string& name_in, int64_t* dataRef_in):
-        AbstractDataRefItem(name_in, "int64_t", sizeof(int64_t)),
-        dataRef(dataRef_in) {}
-
-    ~DataRefItem() {}
-
-    void getNetworkDataRef(void* data)
-    {
-        *(int64_t*)data = lager_utils::htonll(*reinterpret_cast<int64_t*>(dataRef));
-    }
-
-private:
-    int64_t* dataRef;
-};
+    *(int16_t*)data = htons(*reinterpret_cast<int16_t*>(dataRef));
+}
 
 template<>
-class DataRefItem<double> : public AbstractDataRefItem
+DataRefItem<uint32_t>::DataRefItem(const std::string& name_in, uint32_t* dataRef_in):
+    AbstractDataRefItem(name_in, "uint32_t", sizeof(uint32_t)),
+    dataRef(dataRef_in) {}
+
+template<>
+void DataRefItem<uint32_t>::getNetworkDataRef(void* data)
 {
-public:
-    DataRefItem(const std::string& name_in, double* dataRef_in):
-        AbstractDataRefItem(name_in, "double", sizeof(double)),
-        dataRef(dataRef_in) {}
+    *(uint32_t*)data = htonl(*reinterpret_cast<uint32_t*>(dataRef));
+}
 
-    ~DataRefItem() {}
+template<>
+DataRefItem<int32_t>::DataRefItem(const std::string& name_in, int32_t* dataRef_in):
+    AbstractDataRefItem(name_in, "int32_t", sizeof(int32_t)),
+    dataRef(dataRef_in) {}
 
-    void getNetworkDataRef(void* data)
-    {
-        *(int64_t*)data = lager_utils::htonll(*reinterpret_cast<int64_t*>(dataRef));
-    }
+template<>
+void DataRefItem<int32_t>::getNetworkDataRef(void* data)
+{
+    *(int32_t*)data = htonl(*reinterpret_cast<int32_t*>(dataRef));
+}
 
-private:
-    double* dataRef;
-};
+template<>
+DataRefItem<uint64_t>::DataRefItem(const std::string& name_in, uint64_t* dataRef_in):
+    AbstractDataRefItem(name_in, "uint64_t", sizeof(uint64_t)),
+    dataRef(dataRef_in) {}
+
+template<>
+void DataRefItem<uint64_t>::getNetworkDataRef(void* data)
+{
+    //*(uint64_t*)data = lager_utils::htonll(*reinterpret_cast<uint64_t*>(dataRef));
+    *(uint64_t*)data = *reinterpret_cast<uint64_t*>(dataRef);
+}
+
+template<>
+DataRefItem<int64_t>::DataRefItem(const std::string& name_in, int64_t* dataRef_in):
+    AbstractDataRefItem(name_in, "int64_t", sizeof(int64_t)),
+    dataRef(dataRef_in) {}
+
+template<>
+void DataRefItem<int64_t>::getNetworkDataRef(void* data)
+{
+    *(int64_t*)data = *reinterpret_cast<int64_t*>(dataRef);
+}
+
+template<>
+DataRefItem<double>::DataRefItem(const std::string& name_in, double* dataRef_in):
+    AbstractDataRefItem(name_in, "double", sizeof(double)),
+    dataRef(dataRef_in) {}
+
+template<>
+void DataRefItem<double>::getNetworkDataRef(void* data)
+{
+    *(double*)data = *reinterpret_cast<double*>(dataRef);
+}
 
 #endif
